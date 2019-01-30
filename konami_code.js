0c1a50759a194acdc16c1195af015cdde12d661f
Chris Franklin <cf73021@gmail.com>
2019-01-30 19:00:12 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,7 @@ function init() {
   document.body.addEventListener('keydown', onKeyDownHandler);
 
   function onKeyDownHandler(e){
-    const key = e.key
+    const key = parseInt(e.detail || e.which)
       if (key === codes([index])){
         index++;
       if (index === codes.length){
